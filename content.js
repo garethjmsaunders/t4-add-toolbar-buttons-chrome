f8addaf2428f5587f8bd3a0b9a145c729c3399f0
Gareth J M Saunders <gareth@garethjmsaunders.co.uk>
2025-07-15 21:30:45 +0100

    FEAT Add new buttons
    Add button for Content Type Usage
    Add button for Clear the Cache

--- content.js
+++ content.js
@@ -1,10 +1,49 @@
 (function injectT4ToolbarIcons() {
+  /*                name
+                    link
+                    font awesome icon
+                    label
+                    tooltip
+  */
   const buttonDefinitions = {
-    media: ['media', 'media-library', 'fa fa-image', 'Media Library', 'Go to the Media Library'],
-    mediaconfig: ['mediaconfig#types', 'mediaconfig', 'fa fa-gear', 'Media Configuration', 'Go to the Media Configuration'],
-    user: ['user', 'user-management', 'fa fa-user', 'User Management', 'Go to User Management'],
-    group: ['group', 'group-management', 'fa fa-users', 'Group Management', 'Go to Group Management'],
+    media:        [
+      '/terminalfour/page/media',
+      'media-library',
+      'fa fa-image',
+      'Media Library',
+      'Go to the Media Library'],
+    mediaconfig:  [
+      '/terminalfour/page/mediaconfig#types',
+      'mediaconfig',
+      'fa fa-sliders',
+      'Media Configuration',
+      'Go to the Media Configuration'],
+    user:         [
+      '/terminalfour/page/user',
+      'user-management',
+      'fa fa-user',
+      'User Management',
+      'Go to User Management'],
+    group:        [
+      '/terminalfour/page/group',
+      'group-management',
+      'fa fa-users',
+      'Group Management',
+      'Go to Group Management'],
+    contenttypeusage: [
+      '/terminalfour/page/contenttypeusage',
+      'content-type-usage',
+      'fa fa-puzzle-piece',
+      'Content Type Usage',
+      'View Content Type Usage'],
+    clearcache: [
+      '/terminalfour/SiteManager?ctfn=cache',
+      'clear-cache',
+      'fa fa-rotate-right',
+      'Clear the Cache',
+      'Clear the site cache'],
   };
+
 
   chrome.storage.sync.get(['t4toolbar'], (data) => {
     const settings = data.t4toolbar || {};
